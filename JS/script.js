58edec42ee625e3412a069a08ded21387fde3645
kikigoestech <iamtummo13>
2022-07-07 22:13:55 -0400

    final edits

--- JS/script.js
+++ JS/script.js
@@ -1,74 +1,190 @@
-// dom elements
-
-var questionsEL = document.querySelector("#questions");
+// DOM elements
+var questionsEl = document.querySelector("#questions");
 var timerEl = document.querySelector("#time");
-var choicesEL = document.querySelector("#choices");
+var choicesEl = document.querySelector("#choices");
 var submitBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
-var initialsEL = document.querySelector("#initials");
-var feedbackEL = document.querySelector("#feedback");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
 
 // quiz state variables
-
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 
 function startQuiz() {
-    var startscreenEl = document.getElementById("start-screen");
-    startscreenEl.setAttribute("class", "hide");
+  // hide start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
 
-    questionsEL.removeAttribute("class");
+  // un-hide questions section
+  questionsEl.removeAttribute("class");
 
-    timerId = setInterval(clockTick, 1000);
-    timerEl.textContent= time;
-    
-    getQuestion();
+  // start timer
+  timerId = setInterval(clockTick, 1000);
+
+  // show starting time
+  timerEl.textContent = time;
+
+  getQuestion();
 }
 
 function getQuestion() {
-    // get current question from array
-    var currentQuestion = questions[currentQuestionIndex];
+  // get current question object from array
+  var currentQuestion = questions[currentQuestionIndex];
 
-    var titleEL = document.getElementById("question-title");
-    titleEL.textContent = currentQuestion.title;
+  // update title with current question
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
 
-    choicesEL.innerHTML = "";
-//loop over choices
-currentQuestion.choices.forEach(function(choice, i) {
+  // clear out any old question choices
+  choicesEl.innerHTML = "";
 
+  // loop over choices
+  currentQuestion.choices.forEach(function(choice, i) {
+    // create new button for each choice
     var choiceNode = document.createElement("button");
     choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute("value", "choice");
+    choiceNode.setAttribute("value", choice);
 
-    choice.textContent = i + 1 + "." + choice;
+    choiceNode.textContent = i + 1 + ". " + choice;
 
-    //attach click event listener to each choice
+    // attach click event listener to each choice
     choiceNode.onclick = questionClick;
 
-    //display on page
-    choicesEL.appendChild(choiceNode);
-}); 
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+  });
 }
 
+function questionClick() {
+  // check if user guessed wrong
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    // penalize time
+    time -= 15;
 
+    if (time < 0) {
+      time = 0;
+    }
+    // display new time on page
+    timerEl.textContent = time;
+    feedbackEl.textContent = "Wrong!";
+    feedbackEl.style.color = "red";
+    feedbackEl.style.fontSize = "400%";
+  } else {
+    feedbackEl.textContent = "Correct!";
+    feedbackEl.style.color = "green";
+    feedbackEl.style.fontSize = "400%";
+  }
 
+  // flash right/wrong feedback
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
 
+  // next question
+  currentQuestionIndex++;
 
+  // time checker
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
 
+function quizEnd() {
+  // stop timer
+  clearInterval(timerId);
 
+  // show end screen
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
 
+  // show final score
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
 
+  // hide questions section
+  questionsEl.setAttribute("class", "hide");
+}
 
+function clockTick() {
+  // update time
+  time--;
+  timerEl.textContent = time;
 
+  // check if user ran out of time
+  if (time <= 0) {
+    quizEnd();
+  }
+}
 
+function saveHighscore() {
+  // get value of input box
+  var initials = initialsEl.value.trim();
 
+  if (initials !== "") {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
 
+    // format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials
+    };
 
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
+    // redirect to next page
+    window.location.href = "score.html";
+  }
+}
 
+function checkForEnter(event) {
+  // "13" represents the enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
 
+// submit initials
+submitBtn.onclick = saveHighscore;
 
+// start quiz
 startBtn.onclick = startQuiz;
 
-initialsEL.onkeyup = checkForEnter; 
+initialsEl.onkeyup = checkForEnter;
+
+// list of all questions, choices and answers
+let questions = [
+  {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "Booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  {
+      title:"The condition in an if/else statement is enclosed with _____",
+      choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+      answer: "parenthesis"
+  },
+  {
+      title: "Arrays in JavaScript can be used to store ______",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above"
+  },
+  {
+      title: "String Values must be enclosed within ____ when being assigned to variables",
+      choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+      answer: "quotes"
+  },
+  {
+      title: "A very useful tool used during the develpoment and debuggin for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+      answer: "console.log"
+  }
+];
